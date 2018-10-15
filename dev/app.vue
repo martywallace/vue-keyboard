d62cd3a5ec9140afb72b89d143c9ce7489650ae3
dufia <moskalkonrad@gmail.com>
2018-10-15 16:25:28 +0100

    add webpack (dev, dist)

---
+++ dev/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <keyboard layouts="abc123|xyz456|{space:space}"></keyboard>
+  </div>
+</template>
+
+<script>
+import keyboard from '../src/keyboard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    keyboard,
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import keyboard from './keyboard.vue';
+
+export {
+  keyboard
+}
